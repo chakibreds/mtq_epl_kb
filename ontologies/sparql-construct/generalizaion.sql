e9fe1336e11b77b4367569bc996c7afc25de4848
chakib <celhouiti@gmail.com>
2021-03-17 07:19:39 +0100

    c1c2c3

--- ontologies/sparql-construct/generalizaion.sql
+++ ontologies/sparql-construct/generalizaion.sql
@@ -4,6 +4,18 @@ PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 PREFIX bet: <http://www.semanticweb.org/massy/ontologies/2021/2/untitled-ontology-5#>
 
+
+/* c1 l'equipe joue à domicile */
+CONSTRUCT{
+    ?team bet:c1 ?game
+}
+WHERE{
+    ?team rdf:type bet:Team.
+    ?game rdf:type bet:GameUpComing.
+    ?team bet:playGameHome ?game
+}
+/* c2 l'equipe à un meilleur classement que son adversaire */
+
 CONSTRUCT {
     ?team bet:c2 ?game.
 }
@@ -18,3 +30,20 @@ WHERE {
     ?game bet:teams ?team.
     FILTER(?t != ?team)
 }
+
+CONSTRUCT {
+    ?team bet:c3 ?game.
+}
+WHERE { 
+    ?team rdf:type bet:Team.
+    ?t rdf:type bet:Team.
+    ?game rdf:type bet:GameUpComing.
+    
+    ?team bet:best10RankThan ?t.
+    
+    ?game bet:teams ?t.
+    ?game bet:teams ?team.
+    FILTER(?t != ?team)
+}
+
+
